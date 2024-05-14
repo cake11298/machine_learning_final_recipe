59aa1c2f183dc8d52581d8c05283014069fa1721
cake11298 <cake11298@gmail.com>
2024-05-14 19:12:06 +0800

    Adding Recognize function

---
+++ recognize.py
@@ -0,0 +1,44 @@
+import base64
+import requests
+
+api_key = "YOUR_API"
+# 0
+
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+image_path = "189.jpeg"
+
+base64_image = encode_image(image_path)
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+  "model": "gpt-4-turbo",
+  "messages": [
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "請分辨出此圖片中所有食材，並且使用英文列出，格式需要遵照以下回傳，並且不要回傳別的，例如\"1. shrimp 2. tomato 3. XXX\"，若沒有食材請回覆No"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+          }
+        }
+      ]
+    }
+  ],
+  "max_tokens": 300
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json()['choices'][0]['message']['content'])
