6625ca4f7ddb825ed19001028c496f2487233ad6
cake11298 <cake11298@gmail.com>
2024-05-14 19:27:00 +0800

    changing the parsing function make the parsing function can find all the information on the special web

--- parsing.py
+++ parsing.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 # 初始化 WebDriver
-driver_path = "chromedriver.exe"
-service = Service(driver_path)
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # 您要抓取的食譜網址
 url = "https://www.food.com/recipe/bourbon-chicken-45809"
@@ -26,11 +25,13 @@ data['content'] = content
 pic_url = driver.find_element(By.CLASS_NAME, "primary-image.svelte-wgcq7z").find_element(By.TAG_NAME, "img").get_attribute('src')
 data['pics'] = pic_url
 
-# 抓取所有成分
+# 抓取所有成分，包括數量和描述
 ingredients = []
-ingredients_elements = driver.find_elements(By.CSS_SELECTOR, "ul.ingredient-list.svelte-1dqq0pw li span.ingredient-text.svelte-1dqq0pw")
+ingredients_elements = driver.find_elements(By.CSS_SELECTOR, "ul.ingredient-list.svelte-1dqq0pw li")
 for element in ingredients_elements:
-    ingredients.append(element.text)
+    quantity = element.find_element(By.CLASS_NAME, "ingredient-quantity.svelte-1dqq0pw").text
+    text = element.find_element(By.CLASS_NAME, "ingredient-text.svelte-1dqq0pw").text
+    ingredients.append(quantity + " " + text)  # 數量和成分描述合併成一個字符串
 data['ingredient'] = ingredients
 
 # 打印出抓取的資料
