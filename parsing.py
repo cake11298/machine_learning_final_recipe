29cae5e001dddd9413a767f4deca1da8de84193f
ncumibrs <ncumbirs2024@gmail.com>
2024-05-15 22:35:03 +0800

    Parsing Function completed

--- parsing.py
+++ parsing.py
@@ -1,42 +1,61 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+import json
 
-# 初始化 WebDriver
 driver_path = "chromedriver.exe"
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 
-# 您要抓取的食譜網址
-url = "https://www.food.com/recipe/bourbon-chicken-45809"
+url = "https://www.food.com/recipe/spinach-and-cheese-stuffed-chicken-breast-rsc-495271"
 driver.get(url)
 
-# 創建一個空字典來儲存抓取的資料
 data = {}
 
-# 抓取 Head
 head = driver.find_element(By.CLASS_NAME, "svelte-1muv3s8").text
 data['HEAD'] = head
 
-# 抓取 Content
 content = driver.find_element(By.CLASS_NAME, "text.svelte-1aswkii.truncated").text
 data['content'] = content
 
-# 抓取圖片網址
 pic_url = driver.find_element(By.CLASS_NAME, "primary-image.svelte-wgcq7z").find_element(By.TAG_NAME, "img").get_attribute('src')
 data['pics'] = pic_url
 
-# 抓取所有成分，包括數量和描述
 ingredients = []
 ingredients_elements = driver.find_elements(By.CSS_SELECTOR, "ul.ingredient-list.svelte-1dqq0pw li")
 for element in ingredients_elements:
     quantity = element.find_element(By.CLASS_NAME, "ingredient-quantity.svelte-1dqq0pw").text
     text = element.find_element(By.CLASS_NAME, "ingredient-text.svelte-1dqq0pw").text
-    ingredients.append(quantity + " " + text)  # 數量和成分描述合併成一個字符串
-data['ingredient'] = ingredients
+    ingredients.append(quantity + " " + text)
+data['ingredients'] = ingredients
 
-# 打印出抓取的資料
+directions = []
+ind = 1
+directions_section = driver.find_element(By.CLASS_NAME, "directions.svelte-1dqq0pw")
+directions_elements = directions_section.find_elements(By.CSS_SELECTOR, "ul.direction-list.svelte-1dqq0pw li")
+for element in directions_elements:
+    direction = str(ind) + '. ' + element.text
+    directions.append(direction)
+    ind += 1
+data['directions'] = directions
+
+nutrition_script = driver.find_element(By.XPATH, "//script[@type='application/ld+json']")
+nutrition_data = json.loads(nutrition_script.get_attribute('innerHTML'))
+nutrition_info = nutrition_data['nutrition']
+
+nutrition = [
+    "calories " + str(nutrition_info['calories']) + "g",
+    "fatContent " + str(nutrition_info['fatContent']) + "g",
+    "saturatedFatContent " + str(nutrition_info['saturatedFatContent']) + "g",
+    "cholesterolContent " + str(nutrition_info['cholesterolContent']) + "mg",  # mg單位
+    "sodiumContent " + str(nutrition_info['sodiumContent']) + "mg",  # mg單位
+    "carbohydrateContent " + str(nutrition_info['carbohydrateContent']) + "g",
+    "fiberContent " + str(nutrition_info['fiberContent']) + "g",
+    "sugarContent " + str(nutrition_info['sugarContent']) + "g",
+    "proteinContent " + str(nutrition_info['proteinContent']) + "g"
+]
+data['nutrition'] = nutrition
+
 print(data)
 
-# 關閉瀏覽器
 driver.quit()
