4eb9b3d58c823b0b1ac388e9ad9ec8851bd93c52
frank931023 <frank931023@gmail.com>
2024-05-31 20:53:39 +0800

    vgg16 model added

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, jsonify
 import test
+from flask import request
+from werkzeug.utils import secure_filename
+import os
 app = Flask(__name__)
 
 articles = [
@@ -21,6 +24,20 @@ def refresh():
 def get_recipe():
     print(articles[0])
     return jsonify(articles[0])
+
+app.config['UPLOAD_FOLDER'] = './uploads'
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part', 400
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file', 400
+    if file:
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- recognize.py
+++ recognize.py
@@ -9,7 +9,52 @@ def encode_image(image_path):
 
 image_path = "189.jpeg"
 
-base64_image = encode_image(image_path)
+from keras.applications import VGG16
+
+# Load the VGG16 model
+model = VGG16(weights="imagenet")
+model.summary() # show the imagenet model architecture and the number of parameters
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import keras.utils as image_utils
+
+# Show image function
+def show_image(image_path):
+    if "http" in image_path:
+        image_path = image_utils.get_file(origin=image_path)
+    image = mpimg.imread(image_path)
+    plt.imshow(image)
+
+from keras.applications.vgg16 import preprocess_input
+
+# Load and pre-process image function for VGG16 model
+def load_and_process_image(image_path):
+    if "http" in image_path:
+        image_path = image_utils.get_file(origin=image_path)
+    image_s = image_utils.load_img(image_path, target_size=(224, 224)) # load image and resize it to 224x224
+    image_s_array = image_utils.img_to_array(image_s) # change the format of the image to array
+    # single picture, so expand_dims
+    image_s_array_reshape = image_s_array.reshape(1, 224, 224, 3) # reshape the image to 4D array
+    # ImageNet preprocessing
+    image_forVGG16 = preprocess_input(image_s_array_reshape) # preprocess the image for VGG16 model
+    return image_forVGG16
+  
+from keras.applications.vgg16 import decode_predictions
+
+# Make predictions using the VGG16 pre-trained model
+def readable_prediction(image_path):
+    # Show image
+    show_image(image_path)
+    # Load and pre-process image
+    image = load_and_process_image(image_path)
+    # Make predictions using the VGG16 pre-trained model, but it is not human-readable
+    predictions = model.predict(image) 
+    # Print predictions in readable form (get top 3 predictions)
+    # Get the name of top 1 prediction and change the "_" into " "
+    top_predictions = decode_predictions(predictions, top=1)
+    top1prediction_name = top_predictions[0][0][1].replace('_', ' ')
+    return top1prediction_name
 
 headers = {
   "Content-Type": "application/json",
@@ -24,13 +69,12 @@ payload = {
       "content": [
         {
           "type": "text",
-          "text": "請分辨出此圖片中所有食材，並且使用英文列出，格式需要遵照以下回傳，並且不要回傳別的，例如\"1. shrimp 2. tomato 3. XXX\"，若沒有食材請回覆No"
+          # "text": "請分辨出此圖片中所有食材，並且使用英文列出，格式需要遵照以下回傳，並且不要回傳別的，例如\"1. shrimp 2. tomato 3. XXX\"，若沒有食材請回覆No"
+          "text": "問問題 但好像也不用問問題?"
         },
         {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{base64_image}"
-          }
+          "type": "text",
+          "text": readable_prediction(image_path)
         }
       ]
     }
@@ -40,4 +84,4 @@ payload = {
 
 response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
 
-print(response.json()['choices'][0]['message']['content'])
+# print(response.json()['choices'][0]['message']['content'])
